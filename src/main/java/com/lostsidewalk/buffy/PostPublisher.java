1841fe3e4cae0bbea6cd9e408bc92023dc9d12ce
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2023-06-12 11:39:51 -0500

    Upgraded dependencies; renamed feed -> queue; renamed query -> subscription

--- src/main/java/com/lostsidewalk/buffy/PostPublisher.java
+++ src/main/java/com/lostsidewalk/buffy/PostPublisher.java
@@ -5,10 +5,10 @@ import com.lostsidewalk.buffy.publisher.FeedPreview;
 import com.lostsidewalk.buffy.publisher.Publisher;
 import com.lostsidewalk.buffy.publisher.Publisher.PubFormat;
 import com.lostsidewalk.buffy.publisher.Publisher.PubResult;
-import com.lostsidewalk.buffy.feed.FeedDefinition;
-import com.lostsidewalk.buffy.feed.FeedDefinitionDao;
 import com.lostsidewalk.buffy.post.StagingPost;
 import com.lostsidewalk.buffy.post.StagingPostDao;
+import com.lostsidewalk.buffy.queue.QueueDefinition;
+import com.lostsidewalk.buffy.queue.QueueDefinitionDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -32,7 +32,7 @@ import static org.apache.commons.lang3.time.FastDateFormat.getDateTimeInstance;
 public class PostPublisher {
 
     @Autowired
-    FeedDefinitionDao feedDefinitionDao;
+    QueueDefinitionDao queueDefinitionDao;
 
     @Autowired
     StagingPostDao stagingPostDao;
@@ -49,16 +49,16 @@ public class PostPublisher {
     }
 
     @SuppressWarnings("unused")
-    public List<PubResult> publishFeed(String username, Long feedId) throws DataAccessException, DataUpdateException {
-        FeedDefinition feedDefinition = this.feedDefinitionDao.findByFeedId(username, feedId);
-        if (feedDefinition == null) {
-            log.error("Unable to locate feed definition with Id={}", feedId);
+    public List<PubResult> publishFeed(String username, Long queueId) throws DataAccessException, DataUpdateException {
+        QueueDefinition queueDefinition = this.queueDefinitionDao.findByQueueId(username, queueId);
+        if (queueDefinition == null) {
+            log.error("Unable to locate queue definition with Id={}", queueId);
             return emptyList();
         }
         // fetch all pub-pending posts..
-        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, feedId);
+        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, queueId);
         // fetch all currently published posts..
-        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByFeed(username, feedId);
+        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByQueue(username, queueId);
         // ..`toPublish' is the union of both sets
         List<StagingPost> toPublish = new ArrayList<>(pubCurrent.size() + pubPending.size());
         toPublish.addAll(pubCurrent);
@@ -68,36 +68,36 @@ public class PostPublisher {
         // invoke doPublish on ea. publisher and collect the results in a list
         Date pubDate = new Date();
         List<PubResult> publicationResults = publishers.stream()
-                .map(publisher -> doPublish(publisher, feedDefinition, toPublish, pubDate))
+                .map(publisher -> doPublish(publisher, queueDefinition, toPublish, pubDate))
                 .collect(toList());
         // mark ea. post as pub complete (clear the status and set is_published to true)
         for (StagingPost n : pubPending) {
             this.stagingPostDao.markPubComplete(username, n.getId());
         }
         // fetch all de-pub-pending posts..
-        List<StagingPost> depubPending = stagingPostDao.getDepubPending(username, feedId);
+        List<StagingPost> depubPending = stagingPostDao.getDepubPending(username, queueId);
         // unmark ea. de-pub-pending post as pub complete (set the status to REVIEW and set is_published to false)
         for (StagingPost n : depubPending) {
             this.stagingPostDao.clearPubComplete(username, n.getId());
         }
-        feedDefinitionDao.updateLastDeployed(username, feedId, pubDate);
-        log.info("Published {} articles, de-published {} articles, for feedId={} ({}) at {}, result={}",
-                size(pubPending), size(depubPending), feedId, username, pubDate, publicationResults);
+        queueDefinitionDao.updateLastDeployed(username, queueId, pubDate);
+        log.info("Published {} articles, de-published {} articles, for queueId={} ({}) at {}, result={}",
+                size(pubPending), size(depubPending), queueId, username, pubDate, publicationResults);
 
         return publicationResults;
     }
 
     @SuppressWarnings("unused")
-    public void unpublishFeed(String username, Long feedId) throws DataAccessException, DataUpdateException {
-        FeedDefinition feedDefinition = this.feedDefinitionDao.findByFeedId(username, feedId);
-        if (feedDefinition == null) {
-            log.error("Unable to locate feed definition with Id={}", feedId);
+    public void unpublishFeed(String username, Long queueId) throws DataAccessException, DataUpdateException {
+        QueueDefinition queueDefinition = this.queueDefinitionDao.findByQueueId(username, queueId);
+        if (queueDefinition == null) {
+            log.error("Unable to locate queue definition with Id={}", queueId);
             return;
         }
         // fetch all pub-pending posts..
-        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, feedId);
+        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, queueId);
         // fetch all currently published posts..
-        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByFeed(username, feedId);
+        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByQueue(username, queueId);
         // ..`toUnpublish' is the union of both sets
         List<StagingPost> toUnpublish = new ArrayList<>(pubCurrent.size() + pubPending.size());
         toUnpublish.addAll(pubCurrent);
@@ -106,38 +106,38 @@ public class PostPublisher {
         Date unpubDate = new Date();
         log.info("Post publisher un-publishing {} posts at {}", size(toUnpublish), getDateTimeInstance(MEDIUM, MEDIUM).format(unpubDate));
         // de-publish the feed
-        renderedFeedDao.deleteFeedAtTransportIdent(feedDefinition.getTransportIdent());
+        renderedFeedDao.deleteFeedAtTransportIdent(queueDefinition.getTransportIdent());
         // clear the pub complete flag from ea. post
         for (StagingPost n : toUnpublish) {
             this.stagingPostDao.clearPubComplete(username, n.getId());
         }
-        // clear the last deployed timestamp on the feed (it's no longer deployed)
-        feedDefinitionDao.clearLastDeployed(username, feedDefinition.getId());
+        // clear the last deployed timestamp on the queue (it's no longer deployed)
+        queueDefinitionDao.clearLastDeployed(username, queueDefinition.getId());
 
-        log.info("Un-published {} articles for feedId={} ({}) at {}", size(toUnpublish), feedId, username, unpubDate);
+        log.info("Un-published {} articles for queueId={} ({}) at {}", size(toUnpublish), queueId, username, unpubDate);
     }
 
     //
     //
     //
 
-    private PubResult doPublish(Publisher publisher, FeedDefinition feedDefinition, List<StagingPost> toPublish, Date pubDate) {
+    private PubResult doPublish(Publisher publisher, QueueDefinition queueDefinition, List<StagingPost> toPublish, Date pubDate) {
         try {
-            return publisher.publishFeed(feedDefinition, toPublish, pubDate);
+            return publisher.publishFeed(queueDefinition, toPublish, pubDate);
         } catch (Exception e) { // publisher *should* handle their own exceptions
             return PubResult.from(publisher.getPublisherId(), singletonList(e), pubDate);
         }
     }
 
     @SuppressWarnings("unused")
-    public List<FeedPreview> doPreview(String username, Long feedId, PubFormat format) throws DataAccessException {
-        FeedDefinition feedDefinition = this.feedDefinitionDao.findByFeedId(username, feedId);
-        if (feedDefinition == null) {
-            log.error("Unable to locate feed definition with Id={}", feedId);
+    public List<FeedPreview> doPreview(String username, Long queueId, PubFormat format) throws DataAccessException {
+        QueueDefinition queueDefinition = this.queueDefinitionDao.findByQueueId(username, queueId);
+        if (queueDefinition == null) {
+            log.error("Unable to locate queue definition with Id={}", queueId);
             return emptyList();
         }
-        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, feedId);
-        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByFeed(username, feedId);
+        List<StagingPost> pubPending = stagingPostDao.getPubPending(username, queueId);
+        List<StagingPost> pubCurrent = stagingPostDao.findPublishedByQueue(username, queueId);
         List<StagingPost> toPublish = new ArrayList<>(pubCurrent.size() + pubPending.size());
         toPublish.addAll(pubCurrent);
         toPublish.addAll(pubPending);
@@ -149,7 +149,7 @@ public class PostPublisher {
                         List<FeedPreview> feedPreviews = p.doPreview(username, toPublish, format);
                         return feedPreviews.stream();
                     } catch (Exception e) {
-                        log.error("Something horrible happened previewing feedId={} due to: {}", feedId, e.getMessage(), e);
+                        log.error("Something horrible happened previewing queueId={} due to: {}", queueId, e.getMessage(), e);
                     }
                     return null;
                 })
